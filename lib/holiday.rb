71ad39014c9ccfc451c661830dbda0f0f3cf5031
Anna Dybas <a.m.dybas@gmail.com>
2019-05-25 18:08:20 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -72,7 +72,7 @@ def all_supplies_in_holidays(holiday_hash)
   # etc.
   holiday_hash.each do |season, holiday|
     puts "#{season.to_s.capitalize!}:"
-    puts "  #{holiday.to_s.strip}"
+    puts "  #{holiday.strip}"
   end
 
 end
