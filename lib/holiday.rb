80133ae0061645a1e29c95459449c4481247a70d
Anna Dybas <a.m.dybas@gmail.com>
2019-05-25 18:24:59 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -87,7 +87,7 @@ def all_holidays_with_bbq(holiday_hash)
   holiday_hash.each do |season, holiday|
     holiday.each do |holiday_day, items|
       if items == "BBQ"
-        all_holidays_with_bbq << items
+        all_holidays_with_bbq << items.compact
       end
     end
   end
