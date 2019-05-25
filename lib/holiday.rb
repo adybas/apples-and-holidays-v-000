9ac37ae391f62049bf41d0e8b62e94fa1e367f8a
Anna Dybas <a.m.dybas@gmail.com>
2019-05-25 17:18:34 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -32,11 +32,11 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
 #  holiday_hash.each do |season, holiday|
 #    if season == :winter
 holiday_hash[:winter].each do
-    #  holiday.each do 
+    #  holiday.each do
         |day, items|
         items << supply
-      end
-    end
+    #  end
+  #  end
   end
 end
 
